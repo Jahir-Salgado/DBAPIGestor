df79598befdaaa6176278cd5bd820f373d603f5c
Jahir Salgado <abielmejia0807@gmail.com>
2024-12-02 00:15:04 -0600

    main

---
+++ lib/Data/DBObjects.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lib\Data;
+
+class DBObjects
+{
+    const SERVER = 1001;
+    const DATABASE = 1002;
+    const SCHEMA = 1003;
+    const TABLE = 1004;
+    const STORE_PROCEDURE = 1005;
+    const FUNCTION = 1006;
+    const VIEW = 1007;
+    const QUERY = 1008;
+    const TABLE_COLUMN = 1009;
+    const PROCEDURE_PARAMETERS = 1010;
+    const FUNCTION_PARAMETER = 1011;
+}
